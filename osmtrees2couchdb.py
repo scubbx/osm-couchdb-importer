a35547cc2d323af4562b2fdcb0081a009e7e0ded
ScubbX <markusmayr@gmx.net>
2015-12-17 23:48:04 +0100

    adaptions, script currently imports ALL nodes but nothing else

--- osmtrees2couchdb.py
+++ osmtrees2couchdb.py
@@ -13,12 +13,14 @@ relations = 0
 
 
 def elementReader(filename):
-    couchPusher = mpcouch.mpcouchPusher("http://gi88.geoinfo.tuwien.ac.at:5984/osmtreesaustria_allhistory",100,threads = False, jobsbuffersizemax = 500)
+    couchPusher = mpcouch.mpcouchPusher("http://gi88.geoinfo.tuwien.ac.at:5984/osmnodesvienna",30000,threads = False, jobsbuffersizemax = 20)
     oldids = []
     
     def gotCompleteEntry(entry):
-        couchPusher.pushData({'data':entry, '_id': entry[0]['version'] + '-' + entry[0]['id']})
+        # couchPusher.pushData({'data':entry, '_id': entry[0]['version'] + '-' + entry[0]['id']})
+        couchPusher.pushData({'data':entry})
         #print(entry)
+        pass
     
     def start_osm_element(name, attrs):
         global currentTreeData
@@ -49,29 +51,42 @@ def elementReader(filename):
         if name == "node":
             #nodes += 1
             #if nodes % 100000 == 0: print("Processed {} OSM nodes".format(nodes))
-            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            """
-            The following checks if we are required to include this doc in any way.
-            This is the case, if it has an id which is contained in the oldversions variabel.
-            """
-            if currentTreeData[0]['id'] in oldids:
-                print("Got old version: {} of {}, adding.".format(len(currentTreeData[0]['version']), currentTreeData[0]['id'] ))
-                gotCompleteEntry(currentTreeData)
-            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
             
             # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             """
-            The uncommented following code is there to make sure only trees are kept.
+            For reduction of the amount of documents, we only include non-tree
+            data, if it represents deleted nodes.
+            The following code is there to make sure only trees are kept.
             """
-            if 'natural' in currentTreeData[1]:
+            #print("visible = {}".format(currentTreeData[0]['visible']))
+            """
+            if len(currentTreeData[1]) == 0:
+                gotCompleteEntry(currentTreeData)
+                #if currentTreeData[0]['visible'] != "true": print(currentTreeData[0]['visible'])
+            elif 'natural' in currentTreeData[1]:
                 if currentTreeData[1]['natural'] == 'tree': # yay, it's a tree !
                     if currentTreeData[0]['version'] != 1:
                         oldids.append(currentTreeData[0]['id'])
                         # print("oldidsremark: {}".format(len(currentTreeData[0]['version'])))
                     gotCompleteEntry(currentTreeData)
+            """
             # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
-            #gotCompleteEntry(currentTreeData)
+            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            """
+            The following checks if we are required to include this doc in any way.
+            This is the case, if it has an id which is contained in the oldversions variabel.
+            """
+            """
+            if currentTreeData[0]['id'] in oldids:
+                print("Got old version: {} of {}, adding.".format(len(currentTreeData[0]['version']), currentTreeData[0]['id'] ))
+                gotCompleteEntry(currentTreeData)
+            """
+            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            
+            gotCompleteEntry(currentTreeData)
         
         elif name == "way":
             #ways += 1
@@ -80,6 +95,8 @@ def elementReader(filename):
             # !!!!!!!!!!!!!!!!!!!!!! DIRTY HACK HERE !!!!!!!!!!!!!!!!!!
             # !!!!!!!!!!!!!!!!!!!! DO NOT TRY AT HOME !!!!!!!!!!!!!!!!!
             couchPusher.finish()
+            quit()
+            pass
             # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             
@@ -87,6 +104,12 @@ def elementReader(filename):
             pass
             #relations += 1
             #if relations % 100000 == 0: print("Processed {} OSM relations".format(relations))
+        
+        elif name == "tag":
+            pass
+        
+        else:
+            print("Unknown element: {}".format(name))
     
     def char_osm_data(data):
         pass
@@ -112,5 +135,5 @@ def elementReader(filename):
 
 if __name__ == '__main__':
     print("running import")
-    elementReader("/datenspeicher/OSM_full_history/austria-history-151207.osh.bz2")
+    elementReader("/datenspeicher/OSM_full_history/vienna-history-151207.osh.bz2")
     print("finished import")
